0de4fa2cff33b68c54eabfbb1e43deaa3967358a
wenchong <990632303@qq.com>
2020-01-06 23:33:48 +0800

    chatroom

---
+++ src/main/java/com/learn/netty/chatroompro/MychatServerHandler.java
@@ -0,0 +1,32 @@
+package com.learn.netty.chatroompro;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleStateEvent;
+
+public class MychatServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+//        super.userEventTriggered(ctx, evt);
+        if(evt instanceof IdleStateEvent) {
+            IdleStateEvent event = (IdleStateEvent) evt;
+            String eventType = null;
+            switch ((event.state())){
+                case READER_IDLE:
+                    eventType="读空闲";
+                    break;
+                case WRITER_IDLE:
+                    eventType = "写空闲";
+                    break;
+                case ALL_IDLE:
+                    eventType = "读写空闲";
+                    break;
+
+            }
+            System.out.println(ctx.channel().remoteAddress() + "超时事件");
+            ctx.channel().close();
+
+        }
+    }
+}
---
+++ src/main/java/com/learn/netty/chatroompro/MychatServerInitializer.java
@@ -0,0 +1,18 @@
+package com.learn.netty.chatroompro;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+public class MychatServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline channelPipeline = ch.pipeline();
+        channelPipeline.addLast(new IdleStateHandler(5,7,10, TimeUnit.SECONDS));
+        channelPipeline.addLast(null);
+    }
+}
