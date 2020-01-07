8fcf742bba02bfbbff6505f190da101322df6ce8
wenchong <990632303@qq.com>
2020-01-08 00:18:38 +0800

    读空闲 写空闲 读写空闲

---
+++ src/main/java/com/learn/netty/websocket/TextWebSocketFrameHandler.java
@@ -0,0 +1,37 @@
+package com.learn.netty.websocket;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+
+import java.time.LocalDateTime;
+
+public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        Channel channel = ctx.channel();
+
+
+        System.out.println("receive messsge  " + msg.text());
+
+        channel.writeAndFlush(new TextWebSocketFrame("服务器时间" + LocalDateTime.now()));
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handadd :" + ctx.channel().id().asLongText());
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handremove " + ctx.channel().id().asLongText());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("exception get it");
+        ctx.close();
+    }
+}
