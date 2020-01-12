ecc4e714a253fcb4597f0137875ebb2122272d06
wenchong <990632303@qq.com>
2020-01-12 22:06:19 +0800

    html websocet

---
+++ src/main/java/com/learn/netty/nio/NioTest.java
@@ -0,0 +1,25 @@
+package com.learn.netty.nio;
+
+import java.nio.IntBuffer;
+import java.security.SecureRandom;
+
+public class NioTest {
+
+    public static void main(String[] args) {
+
+        IntBuffer buffer = IntBuffer.allocate(10);
+
+        for (int i = 0; i < buffer.capacity(); ++i){
+            int randomNumber = new SecureRandom().nextInt(20);
+            buffer.put(randomNumber);
+
+        }
+        buffer.flip();
+
+        while(buffer.hasRemaining()){
+            System.out.println(buffer.get());
+        }
+
+    }
+
+}
