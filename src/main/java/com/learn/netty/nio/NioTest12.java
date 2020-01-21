d86f6fbae962a5cdf1321e82bf308a121c1e2d9f
wenchong <990632303@qq.com>
2020-01-22 00:30:40 +0800

    nio

---
+++ src/main/java/com/learn/netty/nio/NioTest12.java
@@ -0,0 +1,17 @@
+package com.learn.netty.nio;
+
+public class NioTest12 {
+
+    public static void main(String[] args) {
+        int[] ports = new  int[5];
+
+        ports[0] = 5000;
+        ports[1] = 5000;
+        ports[2] = 5000;
+        ports[3] = 5000;
+        ports[4] = 5000;
+
+
+    }
+
+}
