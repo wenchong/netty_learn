630280ff24e9a21802800d8d6d28ca9433e38aa8
wenchong <990632303@qq.com>
2020-01-26 00:51:00 +0800

    nio

--- src/main/java/com/learn/netty/nio/NioTest12.java
+++ src/main/java/com/learn/netty/nio/NioTest12.java
@@ -1,16 +1,103 @@
 package com.learn.netty.nio;
 
+import java.net.InetSocketAddress;
+import java.net.InterfaceAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
 public class NioTest12 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int[] ports = new  int[5];
 
         ports[0] = 5000;
-        ports[1] = 5000;
-        ports[2] = 5000;
-        ports[3] = 5000;
-        ports[4] = 5000;
+        ports[1] = 5001;
+        ports[2] = 5002;
+        ports[3] = 5003;
+        ports[4] = 5004;
 
+        Selector selector = Selector.open();
+
+
+        for (int i = 0 ; i < ports.length;i++){
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.configureBlocking(false);
+
+            InetSocketAddress address = new InetSocketAddress(ports[i]);
+            ServerSocket serverSocket = serverSocketChannel.socket();
+            serverSocket.bind(address);
+
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+            System.out.println("链接端口" + ports[i]);
+        }
+
+        while(true){
+            int numbers = selector.select();
+            System.out.println("numbers " + numbers);
+
+            Set<SelectionKey> selectorKeys = selector.selectedKeys();
+            System.out.println("selectorKeys " + selectorKeys);
+
+
+            Iterator<SelectionKey> iter = selectorKeys.iterator();
+
+            while (iter.hasNext()){
+                SelectionKey selectorKey = iter.next();
+
+                if (selectorKey.isAcceptable()){
+                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel)selectorKey.channel();
+
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+
+                    socketChannel.configureBlocking(false);
+                    socketChannel.register(selector,SelectionKey.OP_READ);
+
+                    iter.remove();
+
+                    System.out.println("获取客户但链接 : " + socketChannel);
+
+
+                }else if(selectorKey.isReadable()){
+                    SocketChannel socketChannel = (SocketChannel)selectorKey.channel();
+                    int bytesRead = 0;
+
+                    while(true){
+                        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+
+                        byteBuffer.clear();
+
+                        int read = socketChannel.read(byteBuffer);
+
+                        if(read <= 0){
+                           break;
+
+                        }
+                        byteBuffer.flip();
+
+                        socketChannel.write(byteBuffer);
+
+                        bytesRead += read;
+
+
+
+                    }
+
+                    System.out.println("读取：" + bytesRead + ",来自于：" + socketChannel);
+
+                    iter.remove();
+
+
+                }
+
+            }
+        }
 
     }
 
